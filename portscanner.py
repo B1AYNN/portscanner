c8616524e1118599e0f842f59ee344e5b966766c
B1AYNN <134533849+B1AYNN@users.noreply.github.com>
2023-05-24 20:33:45 +0400

    portscanner.py

---
+++ portscanner.py
@@ -0,0 +1,20 @@
+import socket
+
+target = "192.168.0.1"  # Skan etmək istədiyiniz hədəf IP ünvanını buraya yazın
+min_port = 1  # Skanın başlayacağı minimum port nömrəsi
+max_port = 65535  # Skanın bitəcəyi maksimum port nömrəsi
+
+def scan_port(port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1)  # Porta qoşulma cəhdi üçün maksimum gözləmə müddəti (saniyə).
+        result = sock.connect_ex((target, port))
+        if result == 0:
+            print("Port {} açık".format(port))
+        sock.close()
+    except socket.error as e:
+        print("Hata oluştu:", str(e))
+
+# Müəyyən edilmiş port diapazonunu skan edin
+for port in range(min_port, max_port+1):
+    scan_port(port)
